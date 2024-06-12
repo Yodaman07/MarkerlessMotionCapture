459f55462d0a39045b83b09bd82dd6908404ba28
Yodaman07 <ayaan.irshad@gmail.com>
2024-06-12 15:08:27 -0700

    possibly works but crashes on an m1 mac

--- main.py
+++ main.py
@@ -2,51 +2,43 @@ import cv2 as cv
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-from renderLandmark import renderLandmarks
 
 # help from https://ai.google.dev/edge/mediapipe/solutions/vision/pose_landmarker/python#live-stream_1
+
+
 model_path = "pose_landmarker_full.task"
 
 PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
+cam = cv.VideoCapture(0)
+
+
+def callback(result: PoseLandmarkerResult, img: mp.Image, timestamp: int):
+    print(result.pose_landmarks)
+    cv.imshow("stream", img.numpy_view())
 
 
 options = vision.PoseLandmarkerOptions(
     base_options=python.BaseOptions(model_asset_path=model_path),
-    output_segmentation_masks=True
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=callback
 )
 
-detector = vision.PoseLandmarker.create_from_options(options)
-a = cv.imread("guy.jpg")
-img = mp.Image(image_format=mp.ImageFormat.SRGB, data=a)
-result = detector.detect(img)
+time = 0
+with vision.PoseLandmarker.create_from_options(
+        options) as poseLandmarker:  # additional help from https://github.com/google-ai-edge/mediapipe/issues/
+    while cam.isOpened():
+        retrieved, frame = cam.read()
+        if not retrieved:
+            print("Stream has likely ended")
+            break
+        time += 1
 
-annotated = renderLandmarks(img.numpy_view(), result)
-cv.imshow("a", cv.cvtColor(annotated, cv.COLOR_RGB2BGR))
-cv.waitKey(0)
+        mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        poseLandmarker.detect_async(mp_img, time)
+        # https://stackoverflow.com/questions/5217519/what-does-opencvs-cvwaitkey-function-do <-- how waitKey works
+        if cv.waitKey(5) == ord("q"):  # gets the unicode value for q
+            break
+
+cam.release()
 cv.destroyAllWindows()
-
-# cam = cv.VideoCapture(0)
-# if not cam.isOpened():
-#     print("Unable to access camera")  # kill the program if the camera is not accessed
-#     cam.release()
-#     exit()
-#
-# while True:
-#     retrieved, frame = cam.read()
-#     if not retrieved:
-#         print("Stream has likely ended")
-#         break
-
-
-        # mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        # landmarker.detect_async(mp_img)
-
-#     cv.imshow("stream", frame)
-#
-#     # https://stackoverflow.com/questions/5217519/what-does-opencvs-cvwaitkey-function-do <-- how waitKey works
-#     if cv.waitKey(1) == ord("q"):  # gets the unicode value for q
-#         break
-#
-# cam.release()
-# cv.destroyAllWindows()
