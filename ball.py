3e2c57ded1c7e900ceefa2be4bab1f8eb9bab8a7
Yodaman07 <ayaan.irshad@gmail.com>
2024-06-15 21:38:09 -0700

    Kinda works a tiny bit better

--- ball.py
+++ ball.py
@@ -5,7 +5,7 @@ import numpy as np
 # object detection code from https://www.youtube.com/watch?v=RaCwLrKuS1w and https://docs.opencv.org/3.4/d4/d70/tutorial_hough_circle.html
 
 prevCircle = None
-dist = lambda x1, y1, x2, y2: (x1 - x2) ** 2 + (y1 - y2) ** 2  # measures the distance between 2 circles
+dist = lambda x1, y1, x2, y2: ((x1 - x2) ** 2) + ((y1 - y2) ** 2)  # measures the distance between 2 circles
 cam = cv.VideoCapture(0)
 
 if not cam.isOpened():
@@ -20,11 +20,11 @@ while True:
         break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blur = cv.medianBlur(gray, 5)  # kernal explanation https://en.wikipedia.org/wiki/Kernel_(image_processing)
+    blur = cv.GaussianBlur(gray, (17,17), 0)  # kernal explanation https://en.wikipedia.org/wiki/Kernel_(image_processing)
     # circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1.2, 100, param1=100, param2=30, minRadius=75,
     #                           maxRadius=400)  # set min and max radius when ready to test with setup
-    rows = gray.shape[0]
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1.2, 100,
+    rows = blur.shape[0]
+    circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1.2, 500,
                               param1=100, param2=30,
                               minRadius=75, maxRadius=400)
 
